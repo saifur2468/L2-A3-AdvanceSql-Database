1e2633b58ed04769ed3405386f0cf9a28bd7d3ba
Your GitHub Username <Your GitHub Email>
2026-06-13 22:07:32 +0600

    insert prblem solve

--- Query.sql
+++ Query.sql
@@ -1,18 +1,17 @@
-create database booking_system;
+create database football_booking;
 
--- create a Users Table
-
+-- create user table
+ 
 CREATE TABLE Users (
-    user_id SERIAL PRIMARY KEY,
+    user_id INT PRIMARY KEY,
     full_name VARCHAR(100) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
-    role VARCHAR(50) NOT NULL CHECK (role IN ('fan', 'admin', 'organizer')),
+    role VARCHAR(50) CHECK (role IN ('Ticket Manager', 'Football Fan')) NOT NULL,
     phone_number VARCHAR(20)
 );
 
+-- create match table
 
-
--- create  matched table
 
 CREATE TABLE Matches (
     match_id INT PRIMARY KEY,
@@ -20,60 +19,125 @@ CREATE TABLE Matches (
     tournament_category VARCHAR(100) NOT NULL,
     base_ticket_price DECIMAL(10,2) NOT NULL CHECK (base_ticket_price >= 0),
 
-match_status VARCHAR(50) NOT NULL CHECK (match_status IN ('upcoming', 'ongoing', 'completed'))
+    match_status VARCHAR(50) NOT NULL CHECK (
+        match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed')
+    )
 );
 
-
-
--- create a booking table
-
-
+-- create booking table 
 CREATE TABLE Bookings (
     booking_id INT PRIMARY KEY,
-    user_id INT NOT NULL,
+  user_id INT NOT NULL,
     match_id INT NOT NULL,
-    seat_number VARCHAR(20),
-   total_cost DECIMAL(10,2) NOT NULL CHECK (total_cost >= 0),
+  seat_number VARCHAR(20),
+    payment_status VARCHAR(50) CHECK (
+       payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+    ),
 
-payment_status VARCHAR(50) CHECK (payment_status IN ('paid', 'pending', 'failed')),
+    total_cost DECIMAL(10,2) NOT NULL CHECK (total_cost >= 0),
 
+    
     FOREIGN KEY (user_id)
-        REFERENCES Users(user_id),
+        REFERENCES Users(user_id)
+        ON DELETE CASCADE,
 
     FOREIGN KEY (match_id)
         REFERENCES Matches(match_id)
+        ON DELETE CASCADE
 );
 
 
+-- user table data insert 
 
--- Data insert to user tabel 
 INSERT INTO Users (user_id, full_name, email, role, phone_number)
 VALUES
-(1, 'Tanvir Rahman', 'tanvir@mail.com', 'fan', '+8801711111111'),
-(2, 'Asif Haque', 'asif@mail.com', 'fan', '+8801722222222'),
-(3, 'Sajjad Rahman', 'sajjad@mail.com', 'organizer', '+8801733333333'),
-(4, 'Jannat Ara', 'jannat@mail.com', 'fan', NULL);
+(1, 'Tanvir Rahman', 'tanvir@mail.com', 'Football Fan', '+8801711111111'),
+(2, 'Asif Haque', 'asif@mail.com', 'Football Fan', '+8801722222222'),
+(3, 'Sajjad Rahman', 'sajjad@mail.com', 'Ticket Manager', '+8801733333333'),
+(4, 'Jannat Ara', 'jannat@mail.com', 'Football Fan', NULL);
 
 
--- data insert matches tabel
 
+-- matches table data insert 
 INSERT INTO Matches (match_id, fixture, tournament_category, base_ticket_price, match_status)
 VALUES
-(1, 'Bangladesh vs India', 'International Friendly', 500.00, 'upcoming'),
-(2, 'Brazil vs Argentina', 'World Cup Qualifier', 1200.00, 'ongoing'),
-(3, 'Germany vs France', 'UEFA Nations League', 800.00, 'completed'),
-(4, 'Spain vs Portugal', 'International Friendly', 700.00, 'upcoming'),
-(5, 'England vs Italy', 'Euro Qualifier', 900.00, 'completed');
+(101, 'Real Madrid vs Barcelona', 'Champions League', 150, 'Available'),
+(102, 'Man City vs Liverpool', 'Premier League', 120, 'Selling Fast'),
+(103, 'Bayern Munich vs PSG', 'Champions League', 130, 'Available'),
+(104, 'AC Milan vs Inter Milan', 'Serie A', 90, 'Sold Out'),
+(105, 'Juventus vs Roma', 'Serie A', 80, 'Available');
 
 
--- data insert into bookings
-INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, total_cost, payment_status)
+-- bookings table data insert 
+INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status, total_cost)
 VALUES
-(1, 1, 1, 'A1', 500.00, 'paid'),
-(2, 2, 1, 'A2', 500.00, 'pending'),
-(3, 1, 2, 'B1', 1200.00, 'paid'),
-(4, 3, 2, 'B2', 1200.00, 'failed'),
-(5, 4, 3, 'C1', 800.00, 'paid'),
-(6, 2, 3, 'C2', 800.00, 'pending'),
-(7, 3, 4, 'D1', 700.00, 'paid'),
-(8, 4, 5, 'D2', 900.00, 'pending');
+(501, 1, 101, 'A-12', 'Confirmed', 150),
+(502, 1, 102, 'B-04', 'Confirmed', 120),
+(503, 2, 101, 'A-13', 'Confirmed', 150),
+(504, 2, 101, NULL, NULL, 150),
+(505, 3, 102, 'C-20', 'Pending', 120);
+
+
+-- Champions League Available matches
+
+SELECT match_id, fixture, base_ticket_price
+FROM Matches
+WHERE tournament_category = 'Champions League'
+  AND match_status = 'Available';
+
+
+-- query 2 
+select user_id,full_name,email
+from users
+where full_name ilike 'Tanvir%'
+or full_name ilike '%Haque%';
+
+-- query 3
+
+select booking_id,user_id,match_id,
+coalesce(payment_status ,'action Required') as systematic_status
+FROM Bookings
+WHERE payment_status IS NULL; 
+
+
+-- query4
+
+SELECT
+    b.booking_id,
+    u.full_name,
+    m.fixture,
+    b.total_cost
+FROM Bookings b
+INNER JOIN Users u ON b.user_id = u.user_id
+INNER JOIN Matches m ON b.match_id = m.match_id;
+
+
+-- query 5 
+SELECT
+  u.user_id,
+  u.full_name,
+b.booking_id
+    
+    
+from users u
+LEFT JOIN Bookings b ON u.user_id = b.user_id
+ORDER BY u.user_id;
+
+
+
+--  query 6 
+select booking_id,match_id,total_cost
+from bookings
+where total_cost > (
+  select avg(total_cost)
+  from bookings
+)
+
+-- query7
+
+SELECT match_id, fixture, base_ticket_price
+FROM Matches
+ORDER BY base_ticket_price DESC
+OFFSET 1
+LIMIT 2;
+
